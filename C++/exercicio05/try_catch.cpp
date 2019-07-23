40a350964aa200b184848b138d2c8c11ecab2c46
Geraldo <geraldovictor@outlook.com>
2019-07-22 23:21:47 -0300

    Try-catch pt2

--- C++/exercicio05/try_catch.cpp
+++ C++/exercicio05/try_catch.cpp
@@ -4,15 +4,29 @@
 
 using namespace std;
 
-int main(){
+double divide(double n1, double n2);
 
-  vector<int>num(6);
+int main(){
+  double n1,n2;
+  //cin >> n1 >> n2;
+  vector<int>num(5);
   try{
-    num.at(7)=10;
-    cout << num[0] << endl;
+    num.at(4)=10;
+    cout << num[4] << endl;
   }catch(exception& e){
     cout << "ERRO: " << e.what() <<endl;
   }
-
+  /*try{
+    cout << divide(n1,n2) << endl;
+  }catch(const char* msg){
+    cout << "ERRO: " << msg <<endl;
+  }*/
   return 0;
+}
+
+double divide(double n1, double n2){
+  if (n2==0){
+    throw "Erro de divisão por 0";
+  }
+  return n1/n2;
 }
